b5112d7748d8bdf019746db19927eacb605f9187
manzurahammed <manzur.ahammed236@gmail.com>
2017-11-30 00:56:10 +0600

    condition

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var app = new Vue({
 		link2:{title:'lol 2',url:'twitter.com'},
 		activelink:'link1',
 		lol:'<h1>Hello World</h1>',
-		lo:["asd",'asdasd']
+		lo:[
+			{name:"asd",exp:2},
+			{name:"asd",exp:5},
+			{name:"asd",exp:4},
+			{name:"asdsdv ",exp:3},
+		]
 	}
 });
