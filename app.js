a4f6c2a648ccb4d6770fc18df51268398386a79f
manzurahammed <manzur.ahammed236@gmail.com>
2017-11-30 00:25:46 +0600

    list

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = new Vue({
 		link1:{title:'lol',url:'facebook.com'},
 		link2:{title:'lol 2',url:'twitter.com'},
 		activelink:'link1',
-		lol:'<h1>Hello World</h1>'
+		lol:'<h1>Hello World</h1>',
+		lo:["asd",'asdasd']
 	}
 });
