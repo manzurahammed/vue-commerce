32f6d350e63f2fc3caedd85d52a0f16357ad6236
manzurahammed <manzur.ahammed236@gmail.com>
2017-12-01 00:29:21 +0600

    computed

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 var app = new Vue({
 	el:"#root",
 	data:{
+		newdata:'',
 		link1:{title:'lol',url:'facebook.com'},
 		link2:{title:'lol 2',url:'twitter.com'},
 		activelink:'link1',
 		lol:'<h1>Hello World</h1>',
-		lo:[
-			{name:"asd",exp:2},
-			{name:"asd",exp:5},
-			{name:"asd",exp:4},
-			{name:"asdsdv ",exp:3},
+		lo:["asd","asdasdada","asdasd"],
+		payments:[
+			{service:'elc',amount:23,paid:true},
+			{service:'Home',amount:30,paid:true},
+			{service:'Bill',amount:25,paid:false},
+			{service:'Gun',amount:40,paid:false}
 		]
+	},
+	methods:{
+		add_data : function(){
+			this.lo.push(this.newdata);
+		},
+		remove:function(i){
+			this.lo.splice(i,1);
+		}
+	},
+	computed : {
+		unpaid : function(){
+			return this.payments.filter(function(p){
+				return !p.paid;
+			});
+		},
+		totalAmount : function(){
+			var sum = 0;
+			for(var i=0;i<this.payments.length;i++){
+				sum += this.payments[i].amount;
+			}
+			return sum;
+		}
 	}
 });
